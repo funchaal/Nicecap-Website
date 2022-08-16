ac18e3366c3adbc52e59d6a0afd235d29d913edd
funchaldikz <funchal.rafael@gmail.com>
2022-08-15 22:34:55 -0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Header from './components/Header.js'
 import iconbeauty from './images/iconbeauty.svg'
+import app_qrcode from './images/frame (3).png'
+import googleplay_ic from './images/googleplay_ic.svg'
+import appstore_ic from './images/appstore_ic.svg'
+import playstoreqrcode from './images/playstoreqrcode.svg'
+import appstoreqrcode from './images/appstoreqrcode.svg'
 
 function App() {
   document.title = 'Nicecap'
@@ -13,22 +18,36 @@ function App() {
           <h2>Nicecap</h2>
           <p className='default'>
             fdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d ds
-          <iframe
-            src={`https://www.youtube.com/embed/ZpZaRejnOxE`}
-            frameBorder="0"
-            allow="autoplay"
-            allowFullScreen
-            title="Vídeo base"
-          />
+                <label style={{ marginTop: '40px', fontWeight: 'bold', textDecoration: 'underline' }}>Download app</label>
+            <div className='download'>
+              <div>
+                <button type='button'><img src={googleplay_ic}></img>Play Store</button>
+                <label>Play Store QR Code</label>
+                <div>
+                  <img src={playstoreqrcode}></img>
+                </div>
+              </div>
+              <div>
+                <button type='button'><img src={appstore_ic}></img>App Store</button>
+                <label>App Store QR Code</label>
+                <div>
+                  <img src={appstoreqrcode}></img>
+                </div>
+              </div>
+            </div>
           </p>
+          <div className='divisor'></div>
+            <p className='default'>
+              <iframe
+                src={`https://www.youtube.com/embed/ZpZaRejnOxE`}
+                frameBorder="0"
+                allow="autoplay"
+                allowFullScreen
+                title="Vídeo base"
+              />
+            </p>
           <h2>Sobre o vídeo</h2>
-          <p className='default'>fdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d dsfdsfsdfsdf fd fsd a fda fasf d ds</p>
-        </div>
-        <div className='side right'>
-          <div>
-            <img src={iconbeauty}></img>
-            <button><a></a></button>
-          </div>
+          <p className='default'>Nesse vídeo podemos notar que mesmo com a muleta para identificar obstáculos a sua volta, o deficiente ainda sofreu uma colisão com o caminhão por ter a parte superior do seu corpo desprotegida.</p>
         </div>
       </div>
     </main>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,11 @@ function Header() {
     return (
         <div id='header'>
             <span>NICECAP</span>
+            <div class='box'>
+                <button type='button'>Sobre</button>
+                <button type='button'>Autores</button>
+                <button type='button' className='on'>Contato</button>
+            </div>
         </div>
     )
 }
