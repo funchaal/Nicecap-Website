79efeca9d8652766059a3d40f5738ddb1d0e41ea
funchaldikz <funchal.rafael@gmail.com>
2022-08-22 18:24:32 -0300

    small changes

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
           <div className='text-divisor'></div>
           <div className='default'>
             <p>
-          A proposta se baseia em um sistema com sensor de aproximação acoplado a um boné que alerta o usuário com um sinal sonoro ao aproximar de um obstáculo. O sistema ainda conta com um app próprio para personalização do usuário. Uma vez que cerca de 22% da população santista tem algum tipo de deficiência, sendo que 54% desses são deficientes visuais, este projeto será útil para promover maior segurança aos deficientes ao se locomoverem no meio urbano.
+          A proposta se baseia em um sistema com sensor de aproximação acoplado a um boné que alerta o usuário com um sinal sonoro ao aproximar de um obstáculo. O sistema ainda conta com um app próprio para personalização do usuário. Uma vez que cerca de <strong>22%</strong> da população santista tem algum tipo de deficiência, sendo que <strong>54%</strong> desses são deficientes visuais, este projeto será útil para promover maior segurança aos deficientes ao se locomoverem no meio urbano.
           </p>
                 <label className='download-label'>Download app
                 <div className='download-divisor'></div>
