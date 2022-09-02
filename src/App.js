d2667262f966b043f3065bc1fafc8f53a65ab85d
funchaldikz <funchal.rafael@gmail.com>
2022-09-02 15:29:18 -0300

    some more changes

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ import model from './images/model.png'
 import { CodeBlock, dracula } from "react-code-blocks";
 import { useState } from 'react'
 import loading_ic from './images/loading-ic.svg'
+import image1 from './images/image 1.jpeg'
+import image2 from './images/image 2.jpeg'
+import image3 from './images/image 3.jpeg'
+import image4 from './images/image 4.jpeg'
 
 function App() {
   document.title = 'Nicecap'
@@ -152,7 +156,7 @@ function App() {
               <h2 id='about'>Sobre</h2>
           <div className='text-divisor'></div>
               <div className='default'>
-              <p>Nesse vídeo podemos notar que mesmo com a muleta para identificar obstáculos a sua volta, o deficiente ainda sofreu uma colisão com o caminhão por ter a parte superior do seu corpo desprotegida.</p></div>
+              <p>O Nicecap é um dispositivo que tem como objetivo proporcionar maior segurança e acessibiidade na locomoção de defisientes visuais no ambiente urbano. A ideia veio com base no video a seguir enquanto discutiamos qual seria o projeto que desenvolveriamos para a feira de ciências.</p></div>
             <div className='default'>
               <iframe
                 src={`https://www.youtube.com/embed/ZpZaRejnOxE`}
@@ -163,16 +167,22 @@ function App() {
               />
             </div>
             <div className='default'>
-            <p>Nesse vídeo podemos notar que mesmo com a muleta para identificar obstáculos a sua volta, o deficiente ainda sofreu uma colisão com o caminhão por ter a parte superior do seu corpo desprotegida.</p></div>
+            <p>Nesse vídeo podemos notar que mesmo com a muleta para identificar obstáculos a sua volta, o deficiente ainda sofreu uma colisão com o caminhão por ter a parte superior do seu corpo desprotegida. Dessa forma, o Nicecap é útil para evitar esse tipo de incidente.</p>
+            <p>O problema com obstáculos para a lomoção urbana é bem expressivo. Como se pode ver nas imagens, a quantidade de caminhões parados na rua em um raio de 2 quadras do bairro é bem grande, além de lixeiras altas que são também bem comuns.</p>
+            <img src={image1}></img>
+            <img src={image2}></img>
+            <img src={image3}></img>
+            <img src={image4}></img>
+            </div>
             <div className='default'>
             <span>Como foi feito
                 <div className='download-divisor'></div>
             </span>
-            <p>Nesse vídeo podemos notar que mesmo com a muleta para identificar obstáculos a sua volta, o deficiente ainda sofreu uma colisão com o caminhão por ter a parte superior do seu corpo desprotegida.</p>
+            <p>A primeira etapa do desenvolvimento foi criar um modelo digital no TinkerCAD do circuito que estruturaria o projeto.</p>
               <img src={model}></img>
-              <p>Nesse vídeo podemos notar que mesmo com a muleta para identificar obstáculos a sua volta, o deficiente ainda sofreu uma colisão com o caminhão por ter a parte superior do seu corpo desprotegida.</p>
+              <p>Depois de planejado, foram comprados os componentes e quando estavam em mãos, o protótipo foi montado e foi criado um código inicial em C++ para interagir com o sensor, os leds, o alto-falante e o arduino.</p>
               <img src={proto}></img>
-              <p>Nesse vídeo podemos notar que mesmo com a muleta para identificar obstáculos a sua volta, o deficiente ainda sofreu uma colisão com o caminhão por ter a parte superior do seu corpo desprotegida.</p>
+              <p>Depois de um tempo veio a ideia de criar um app também para customizar as funções do dispositivo de forma intuitiva. Criado o app, a etapa final praticamente era remontar o dispositivo reduzindo a quantidade de fios, tornando-o compacto e mais agradável, além de fazer melhorias no código acrescentando por exemplo um algoritmo de variancia para detectar se a pessoa está andando ou parada, para evitar ficar apitando enquanto a pessoa estiver em repouso.</p>
               </div>
             <div className='divisor'></div>
             <h2 id='contact' name='contact'>Contato</h2>
