c03048b520f0bf75752056cc7c6b7461e6f69b3d
Rafael <rafael.funchal@outlook.com>
2022-11-04 09:42:35 -0300

    correction

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import logo from './images/logo.svg';
 import './App.css';
 
 // import Home from './sections/Home.js'
-import Reason from './sections/Home_2.js'
-import Base from './sections/Home_3.js'
 import Contact from './pages/Contact.js'
-import Autors from './sections/Home_4.js'
-import Problem from './sections/Home_5.js'
 
 import Home from './pages/Home.js'
 import Support from './pages/Support.js'
-import Header from './Components/Header.js'
+import Header from './components/Header.js'
 
 import React from "react";
 import ReactDOM from "react-dom/client";
